0fce028d979ffe28f6f2133249c73bc238762760
Henrique Lobato Zago <lobato.zago@gmail.com>
2017-10-01 11:30:23 -0300

    Renamed worked-day model to have ".model.ts" suffix.

--- src/app/worked-days/models/worked-day.model.ts
+++ src/app/worked-days/models/worked-day.model.ts
@@ -1,27 +1,30 @@
 export class WorkedDay {
   id: number;
-  days: Array<string>;
   salary: number;
   total_paid: number;
-  extra: number;
+  extra_costs: number;
   deductions: number;
   observations: string;
-  week_number: number;
   employee_id: number;
+  employee_name: string;
+  week_number: number;
+  worked_days: Array<string>;
+  worked_days_label: string;
 
-  constructor(workedDay?: WorkedDay) {
+  constructor(workedDay?) {
     if (workedDay) {
       this.id = workedDay.id;
-      this.days = workedDay.days;
+      this.worked_days = workedDay.worked_days;
+      this.worked_days_label = workedDay.worked_days_label;
       this.salary = workedDay.salary;
       this.total_paid = workedDay.total_paid;
-      this.extra = workedDay.extra;
+      this.extra_costs = workedDay.extra_costs;
       this.deductions = workedDay.deductions;
       this.observations = workedDay.observations;
       this.week_number = workedDay.week_number;
       this.employee_id = workedDay.employee_id;
     } else {
-      this.days = [];
+      this.worked_days = [];
     }
   }
 }
